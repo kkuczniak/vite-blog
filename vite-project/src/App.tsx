d0c6b7a9d437976c4ce989d813becc0f417fb6ee
kkuczniak <51820616+kkuczniak@users.noreply.github.com>
2022-08-29 23:13:38 +0200

    routing for auth user

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,19 +1,77 @@
+import React, { useEffect, useReducer, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
+import AuthRoute from './components/AuthRoute';
 import routes from './config/routes';
-import HomePage from './pages/home';
+import {
+  initialUserState,
+  UserContextProvider,
+  userReducer,
+} from './context/user';
 
 export interface IApplicationProps {}
 
 const App: React.FunctionComponent<IApplicationProps> = (props) => {
+  const [userState, userDispatch] = useReducer(userReducer, initialUserState);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  // debugging
+  const [authStage, setAuthStage] = useState<string>(
+    'Checking local storage ... '
+  );
+
+  useEffect(() => {
+    setTimeout(() => {
+      CheckLocalStorageForCredentials();
+    }, 1000);
+  }, []);
+
+  const CheckLocalStorageForCredentials = () => {
+    setAuthStage('Checking credentials ... ');
+
+    const fire_token = localStorage.getItem('fire_token');
+
+    if (fire_token === null) {
+      userDispatch({ type: 'logout', payload: initialUserState });
+      setAuthStage('No credentials found');
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    } else {
+      setAuthStage('Credentials found, validating... ');
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    }
+  };
+  const userContextValues = {
+    userState,
+    userDispatch,
+  };
+
+  if (loading) {
+    return <h1>{authStage}</h1>;
+  }
+  //TODO loading animation: ;
+
   return (
-    <Routes>
-      {routes.map((route, index) => {
-        return (
-          <Route key={index} path={route.path} element={<route.element />} />
-        );
-      })}
-    </Routes>
+    <UserContextProvider value={userContextValues}>
+      <Routes>
+        {routes.map((route, index) => {
+          if (route.auth) {
+            <AuthRoute>
+              <Route
+                key={index}
+                path={route.path}
+                element={<route.element />}
+              />
+            </AuthRoute>;
+          }
+          return (
+            <Route key={index} path={route.path} element={<route.element />} />
+          );
+        })}
+      </Routes>
+    </UserContextProvider>
   );
 };
 
---
+++ vite-project/src/components/AuthRoute.tsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import logging from '../config/logging';
+import UserContext from '../context/user';
+
+export interface IAuthRouteProps {}
+
+const AuthRoute: React.FunctionComponent<IAuthRouteProps> = (props) => {
+  const { children } = props;
+
+  const userContext = useContext(UserContext);
+
+  if (userContext.userState.user._id === '') {
+    logging.info('Unauthorized, redirecting.');
+    return <Navigate replace to='/login' />;
+  } else {
+    return <>{children}</>;
+  }
+};
+
+export default AuthRoute;
