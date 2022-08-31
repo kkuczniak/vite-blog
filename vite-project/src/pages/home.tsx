a448df021c452278204f28d52ac376b73c74794f
kkuczniak <51820616+kkuczniak@users.noreply.github.com>
2022-08-31 21:26:54 +0200

    home background and navBar styles fix

--- vite-project/src/pages/home.tsx
+++ vite-project/src/pages/home.tsx
@@ -1,7 +1,18 @@
+import BlogPost from '../components/BlogPost';
+import Navigation from '../components/Navigation';
 import IPageProps from '../interfaces/page';
+import styles from '../styles/home.module.scss';
 
 const HomePage: React.FunctionComponent<IPageProps> = (props) => {
-  return <h1>hello home page</h1>;
+  return (
+    <div className={styles.container}>
+      <Navigation />
+
+      <section className={styles.BlogPostSection}>
+        <BlogPost />
+      </section>
+    </div>
+  );
 };
 
 export default HomePage;
