61387c053fcd9227946dbd9f53ebdc041ef29638
kkuczniak <51820616+kkuczniak@users.noreply.github.com>
2022-08-31 21:25:33 +0200

    authRoute hot fix for interface

--- vite-project/src/components/AuthRoute.tsx
+++ vite-project/src/components/AuthRoute.tsx
@@ -3,7 +3,9 @@ import { Navigate } from 'react-router-dom';
 import logging from '../config/logging';
 import UserContext from '../context/user';
 
-export interface IAuthRouteProps {}
+export interface IAuthRouteProps {
+  children: React.ReactNode;
+}
 
 const AuthRoute: React.FunctionComponent<IAuthRouteProps> = (props) => {
   const { children } = props;
