aaee0dd99e7d54c26c2c400b54f5e31e11486a6b
kkuczniak <51820616+kkuczniak@users.noreply.github.com>
2022-09-07 23:01:35 +0200

    nav bar buttons when user is log

--- vite-project/src/components/Navigation.tsx
+++ vite-project/src/components/Navigation.tsx
@@ -1,24 +1,50 @@
+import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import UserContext, { initialUserState } from '../context/user';
 import styles from '../styles/components/Navigation.module.scss';
 
 export interface INavigationProps {}
 
 const Navigation: React.FunctionComponent<INavigationProps> = (props) => {
+  const userContext = useContext(UserContext);
+  const { user } = userContext.userState;
+
+  const Logout = () => {
+    userContext.userDispatch({ type: 'logout', payload: initialUserState });
+  };
   return (
     <nav className={styles.nav}>
       <NavLink to='/'>
         <p>Game Vite</p>
       </NavLink>
       <div className={styles.leftNav}>
-        <NavLink className={styles.glitch} data-glitch='Posts' to='/posts'>
-          Posts
-        </NavLink>
-        <NavLink className={styles.glitch} data-glitch='Sign Up' to='/login'>
-          Sign Up
-        </NavLink>
-        <NavLink className={styles.glitch} data-glitch='Sign In' to='/login'>
-          Sign In
-        </NavLink>
+        {user._id === '' ? (
+          <>
+            <NavLink
+              className={styles.glitch}
+              data-glitch='Sign Up'
+              to='/login'
+            >
+              Sign Up
+            </NavLink>
+            <NavLink
+              className={styles.glitch}
+              data-glitch='Sign In'
+              to='/login'
+            >
+              Sign In
+            </NavLink>
+          </>
+        ) : (
+          <>
+            <NavLink className={styles.glitch} data-glitch='Posts' to='/posts'>
+              Posts
+            </NavLink>
+            <button className={styles.glitch} onClick={() => Logout()}>
+              Logout
+            </button>
+          </>
+        )}
       </div>
     </nav>
   );
