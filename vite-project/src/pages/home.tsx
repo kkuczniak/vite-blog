610d7825dfe70e4ed989e59e63fdca44d00d9f68
kkuczniak <51820616+kkuczniak@users.noreply.github.com>
2022-09-13 22:17:29 +0200

    blogs calls from server

--- vite-project/src/pages/home.tsx
+++ vite-project/src/pages/home.tsx
@@ -1,15 +1,59 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import BlogPost from '../components/BlogPost';
 import Navigation from '../components/Navigation';
+import config from '../config/config';
+import logging from '../config/logging';
+import IBlog from '../interfaces/blog';
 import IPageProps from '../interfaces/page';
 import styles from '../styles/home.module.scss';
 
 const HomePage: React.FunctionComponent<IPageProps> = (props) => {
+  const [blogs, setBlogs] = useState<IBlog[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    GetAllBlogs();
+  }, []);
+
+  const GetAllBlogs = async () => {
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: `${config.server.url}/blogs`,
+      });
+      if (response.status === 200 || response.status === 304) {
+        let blogs = response.data.blogs as IBlog[];
+        blogs.sort((x, y) => y.updatedAt.localeCompare(x.updatedAt));
+        setBlogs(blogs);
+      }
+    } catch (error) {
+      logging.error(error);
+      setError('Unable to get blogs');
+    } finally {
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    }
+  };
+  if (loading) {
+    return <div>Loading</div>;
+    //TODO: loading component
+  }
   return (
     <div className={styles.container}>
       <Navigation />
 
       <section className={styles.BlogPostSection}>
-        <BlogPost />
+        {blogs.length === 0 && <p>There are no blogs yet</p>}
+        {blogs.map((blog, index) => {
+          return (
+            <div key={index}>
+              <BlogPost />
+            </div>
+          );
+        })}
       </section>
     </div>
   );
